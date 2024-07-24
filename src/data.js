091c541b98fac2afac778e063e523ab1928eae1c
Nathan Lam <nathanl1605@gmail.com>
2024-07-24 15:07:45 +0100

    Setup the necessary data

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        title: "Galaxy",
+        location: "Macau",
+        googleMapsUrl: "https://maps.app.goo.gl/DM3Whv9SM2CYKYic6",
+        startDate: "03 May, 2024",
+        endDate: "27 June 2024",
+        description: `The Galaxy Hotel in Macau is a luxury resort known
+         for its opulent accommodations, extensive leisure facilities,
+          and world-class entertainment. Located in the Cotai district, it features
+           a grand casino, multiple fine dining options, a 
+           sprawling pool area with a wave pool and white sand beach,
+            and an array of shopping outlets. The hotel is part of the larger Galaxy Macau complex, 
+        offering visitors a lavish and comprehensive resort experience.`,
+        imageUrl: "https://d2hyz2bfif3cr8.cloudfront.net/imageRepo/2/0/47/80/768/Galaxy_Macau_Exterior_(Dusk)_O.jpg"
+    },
+    {
+        title: "Citygate Outlet",
+        location: "Hong Kong",
+        googleMapsUrl: "https://maps.app.goo.gl/6waut3fgV5JVuaV3A",
+        startDate: "17 June, 2024",
+        endDate: "21 June 2024",
+        description: `
+        Citygate Outlets in Hong Kong is a popular shopping destination located in Tung Chung, near the airport. It features a wide range of brand-name 
+        stores offering discounted prices on fashion, accessories, sportswear,
+         and home goods. The mall also includes dining options, a cinema,
+          and convenient access to public transportation, making it an ideal
+           spot for both shopping and leisure activities.
+        `,
+        imageUrl: "https://d2hyz2bfif3cr8.cloudfront.net/imageRepo/2/0/47/80/768/Galaxy_Macau_Exterior_(Dusk)_O.jpg"
+    },
+    {
+        title: "The Bund",
+        location: "Shanghai",
+        googleMapsUrl: "https://maps.app.goo.gl/7JtDydwcsJ7Dyrvj6",
+        startDate: "11 July, 2022",
+        endDate: "16 July 2022",
+        description: `
+        The Bund in Shanghai is a famous waterfront area along the Huangpu River,
+         known for its historic colonial-era buildings and stunning skyline views.
+          It features a mix of architectural styles, including Gothic, Baroque,
+           and Art Deco, reflecting Shanghai's international past. The Bund is a
+            popular destination for walking, sightseeing,
+         and experiencing the vibrant culture of the city.
+        `,
+        imageUrl: "https://maps.app.goo.gl/7JtDydwcsJ7Dyrvj6"
+    }
+
+];
